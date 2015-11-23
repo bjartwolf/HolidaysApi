cc794006b156b2884a0d230017cdaec2094bc278
Bjartnes <bjartnes@gmail.com>
2015-11-23 19:51:12 +0100

    trying routes

--- HolidaysApi.Server/Program.fs
+++ HolidaysApi.Server/Program.fs
@@ -3,33 +3,55 @@ open Arachne.Http
 open Freya.Core
 open System 
 open Freya.Machine
+open Freya.Machine.Router
+open Arachne.Uri.Template 
+open Freya.Router
 open Freya.Machine.Extensions.Http
-
 open Aklefdal.Holidays.HttpApi.Holidays
-// Properties
-let mediaTypes = freya { return [ MediaType.Text ] } 
-let methods = freya { return [ GET ] }
-let available = freya { return true } // Handlers
 
-let content _ = freya { 
+let mediaTypes = freya { return [ MediaType.Text ] }
+let methods = freya { return [ GET ] }
+let available = freya { return true }
+
+let easter _ = freya {
     return { Description = { Charset = Some Charset.Utf8
                                        Encodings = None
                                        MediaType = Some MediaType.Text
                                        Languages = None }
              Data = Text.Encoding.UTF8.GetBytes(sprintf "%A" (Seq.toList (Seq.take 5(HolidaysNO 2015)))) } }
-// Resource
-let easter = freyaMachine { using http 
-                            mediaTypesSupported mediaTypes
-                            methodsSupported methods
-                            serviceAvailable available
-                            handleOk content } |> FreyaMachine.toPipeline
+
+let holidays _ = freya {
+    return { Description = { Charset = Some Charset.Utf8
+                                       Encodings = None
+                                       MediaType = Some MediaType.Text
+                                       Languages = None }
+             Data = Text.Encoding.UTF8.GetBytes(sprintf "%A" (Seq.toList (Seq.take 3(HolidaysNO 2013)))) } }
+
+let routeEaster = UriTemplate.Parse "/easter/{year}"
+let routeHolidays = UriTemplate.Parse "/holidays/{country}/{year}"
+
+let handlerEaster = freyaMachine { using http
+                                   mediaTypesSupported mediaTypes
+                                   methodsSupported methods
+                                   serviceAvailable available
+                                   handleOk easter } |> FreyaMachine.toPipeline
+
+let holidaysHandler = freyaMachine { using http
+                                     mediaTypesSupported mediaTypes
+                                     methodsSupported methods
+                                     serviceAvailable available
+                                     handleOk holidays } |> FreyaMachine.toPipeline
+
+let routes =
+    freyaRouter { route (Methods [ GET ]) routeEaster handlerEaster
+                  route (Methods [ GET ]) routeHolidays holidaysHandler } |> FreyaRouter.toPipeline
 
 type EasterServer() =
     member __.Configuration () =
-        OwinAppFunc.ofFreya (easter)
+        OwinAppFunc.ofFreya (routes)
 
 [<EntryPoint>]
-let main _ = 
+let main _ =
     let _ = WebApp.Start<EasterServer> ("http://localhost:7000")
     let _ = System.Console.ReadLine ()
     0
