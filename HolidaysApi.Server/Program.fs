6e957227aff98991a3ef1d6c68bea62f9f38cd53
Bjartnes <bjartnes@gmail.com>
2015-12-06 12:03:37 +0100

    logging stuff

--- HolidaysApi.Server/Program.fs
+++ HolidaysApi.Server/Program.fs
@@ -67,6 +67,14 @@ type EasterServer() =
 [<EntryPoint>]
 let main [| port |] =
     let ip = "127.0.0.1"
+    printf "Trying to start"
+    printf "ip %s %s" ip port
+    let hostname = Dns.GetHostName()
+    printf "host %A" hostname
+    let host = Dns.GetHostEntry(hostname)
+    printf "%A" host
+    for foo in host.AddressList do
+        printf "ip %A" foo 
     printf "ip %s %s" ip port
     let _ = WebApp.Start<EasterServer> (sprintf "http://%s:%s" ip port)
     let _ = System.Console.ReadLine ()
