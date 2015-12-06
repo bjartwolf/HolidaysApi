f16ad3b87af7e3b2b7495f125ea4ce2bcff70852
Bjartnes <bjartnes@gmail.com>
2015-12-06 11:36:52 +0100

    just trying

--- HolidaysApi.Server/Program.fs
+++ HolidaysApi.Server/Program.fs
@@ -66,7 +66,7 @@ type EasterServer() =
 
 [<EntryPoint>]
 let main [| port |] =
-    let ip = "*"
+    let ip = "localhost"
     printf "ip %s %s" ip port
     let _ = WebApp.Start<EasterServer> (sprintf "http://%s:%s" ip port)
     let _ = System.Console.ReadLine ()
