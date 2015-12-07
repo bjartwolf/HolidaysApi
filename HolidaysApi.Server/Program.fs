6814b5d8c38c0d9dfd69405d661d3a8770bc21e2
Bjartnes <bjartnes@gmail.com>
2015-12-07 21:46:43 +0100

    trying 00000

--- HolidaysApi.Server/Program.fs
+++ HolidaysApi.Server/Program.fs
@@ -5,7 +5,7 @@ open HolidaysApi.App
 
 [<EntryPoint>]
 let main [| port |] =
-    let ip = "127.0.0.1"
+    let ip = "0.0.0.0"
     printf "Trying to start"
     printf "ip %s %s" ip port
     let hostname = Dns.GetHostName()
